d8a66fcf90721a242777bff4bbbb7a98b0d5a1ea
Fred Sauer <fredsa@google.com>
2026-02-08 21:23:33 -0800

    cobalt updates

--- src/themes/cobalt.js
+++ src/themes/cobalt.js
@@ -1,8 +1,8 @@
-import {EditorView} from "@codemirror/view"
-import {HighlightStyle, syntaxHighlighting} from "@codemirror/language"
-import {tags as t} from "@lezer/highlight"
+import { EditorView } from "@codemirror/view"
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
+import { tags as t } from "@lezer/highlight"
 
-// 1. Editor UI Styles (The "Chrome")
+// 1. Editor UI Styles
 const cobaltTheme = EditorView.theme({
   "&": {
     color: "white",
@@ -12,7 +12,7 @@ const cobaltTheme = EditorView.theme({
     caretColor: "white"
   },
   "&.cm-focused .cm-cursor": {
-    borderLeftColor: "white"
+    borderLeft: "1px solid white"
   },
   "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, ::selection": {
     backgroundColor: "#b36539 !important"
@@ -23,32 +23,33 @@ const cobaltTheme = EditorView.theme({
     borderRight: "1px solid #aaa"
   },
   ".cm-activeLine": {
-    backgroundColor: "#002D57"
+    backgroundColor: "#003399"
   },
   ".cm-activeLineGutter": {
-    backgroundColor: "#002D57",
+    backgroundColor: "#003399",
     color: "#ffee80"
   },
-}, {dark: true});
+  "&.cm-focused .cm-matchingBracket": {
+    outline: "1px solid grey",
+    color: "white !important",
+    backgroundColor: "transparent"
+  },
+}, { dark: true });
 
 // 2. Syntax Highlighting Styles
 const cobaltHighlightStyle = HighlightStyle.define([
-  {tag: [t.atom, t.bool, t.special(t.variableName)], color: "#845dc4"},
-  {tag: [t.color, t.constant(t.name), t.standard(t.name)], color: "#845dc4"},
-  {tag: [t.definition(t.name), t.separator], color: "white"},
-  {tag: [t.meta, t.comment], color: "#08f"},
-  {tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName], color: "white"},
-  {tag: [t.operator, t.operatorKeyword, t.url, t.escape, t.regexp, t.link, t.special(t.string)], color: "#ff9e59"},
-  {tag: [t.processingInstruction, t.string, t.inserted], color: "#3ad900"},
-  {tag: [t.typeName, t.className, t.number, t.changed, t.annotation, t.modifier, t.self, t.namespace], color: "#ff80e1"},
-  {tag: [t.variableName, t.labelName], color: "#9effff"},
-  {tag: t.emphasis, fontStyle: "italic"},
-  {tag: t.heading, fontWeight: "bold", color: "#ffee80"},
-  {tag: t.invalid, color: "#9d1e15"},
-  {tag: t.keyword, color: "#ffee80"},
-  {tag: t.link, color: "#845dc4", textDecoration: "underline"},
-  {tag: t.strikethrough, textDecoration: "line-through"},
-  {tag: t.strong, fontWeight: "bold"},
+  { tag: t.comment, color: "#ccc" },
+  { tag: t.atom, color: "#845dc4 " },
+  { tag: [t.number, t.attributeName, t.className, t.constant(t.name)], color: "#ff80e1" },
+  { tag: t.keyword, color: "#ffee80" },
+  { tag: t.string, color: "#3ad900" },
+  { tag: t.meta, color: "#ff9d00" },
+  { tag: [t.tagName, t.variableName, t.modifier, t.labelName, t.namespace], color: "#9effff" },
+  { tag: [t.definition(t.variableName), t.typeName, t.className, t.namespace, t.definition(t.propertyName)], color: "white" },
+  { tag: t.bracket, color: "#d8d8d8" },
+  { tag: [t.standard(t.name), t.special(t.string), t.special(t.variableName)], color: "#ff9e59" },
+  { tag: t.link, color: "#845dc4" },
+  { tag: t.invalid, color: "#9d1e15" },
 ]);
 
 // 3. Combined Extension

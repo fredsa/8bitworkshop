233657623780a9d8b41b1323a61ccb8af5a4b54b
Fred Sauer <fredsa@google.com>
2026-02-08 20:40:09 -0800

    fix mbo colors

--- src/themes/mbo.js
+++ src/themes/mbo.js
@@ -12,7 +12,7 @@ export const mboTheme = EditorView.theme({
     caretColor: "#ffffec"
   },
   "&.cm-focused .cm-cursor": {
-    borderLeftColor: "#ffffec"
+    borderLeft: "1px solid #ffffec"
   },
   "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, ::selection": {
     backgroundColor: "rgba(113, 108, 98, .99)"
@@ -20,7 +20,7 @@ export const mboTheme = EditorView.theme({
   ".cm-gutters": {
     backgroundColor: "#4e4e4e",
     color: "#dadada",
-    border: "none"
+    borderRight: "none"
   },
   ".cm-activeLine": {
     backgroundColor: "#494b41"
@@ -30,22 +30,28 @@ export const mboTheme = EditorView.theme({
   },
   ".cm-linenumber": {
     color: "#dadada"
+  },
+  ".cm-matchingBracket": {
+    color: "#33ff33 !important"
   }
 }, {dark: true});
 
 // 2. Syntax Highlighting Styles
 export const mboHighlightStyle = HighlightStyle.define([
-  {tag: [t.atom, t.number, t.bool], color: "#00a8c6"},
-  {tag: [t.keyword, t.operator], color: "#ffb928"},
-  {tag: [t.variableName, t.definition(t.variableName)], color: "#ffffec"},
-  {tag: t.attributeName, color: "#9ddfe9"},
+  {tag: t.keyword, color: "#ffb928"},
+  {tag: [t.name, t.variableName, t.standard(t.name)], color: "#ffffec"},
+  {tag: [t.deleted, t.macroName], color: "#00a8c6"}, // maps to cm-variable-2
+  {tag: [t.processingInstruction, t.string, t.inserted], color: "#b4fdb7"}, // Updated string color
+  {tag: t.number, color: "#33aadd"}, // Updated number color
+  {tag: [t.atom, t.bool, t.special(t.variableName)], color: "#00a8c6"},
+  {tag: [t.propertyName, t.attributeName, t.tagName], color: "#9ddfe9"},
+  {tag: t.definition(t.name), color: "#88eeff"}, // maps to cm-def
+  {tag: t.typeName, color: "#ffb928"}, // maps to cm-variable-3
   {tag: t.bracket, color: "#fffffc", fontWeight: "bold"},
   {tag: t.comment, color: "#95958a"},
-  {tag: t.invalid, color: "#ffffec", borderBottom: "1px solid #636363"},
   {tag: t.link, color: "#f54b07"},
-  {tag: t.propertyName, color: "#9ddfe9"},
-  {tag: t.string, color: "#ffcf6c"},
-  {tag: t.tagName, color: "#9ddfe9"},
+  {tag: t.meta, color: "#aaddaa"},
+  {tag: t.invalid, color: "#ffffec", borderBottom: "1px solid #636363"},
 ]);
 
 // 3. Combined Extension

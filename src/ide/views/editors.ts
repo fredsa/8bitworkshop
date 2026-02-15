727704bdbecf87c991ce60e78efab60236a58836
Fred Sauer <fredsa@google.com>
2026-02-14 17:48:15 -0800

    setText

--- src/ide/views/editors.ts
+++ src/ide/views/editors.ts
@@ -321,23 +321,11 @@ export class SourceEditor implements ProjectView {
   }
 
   setText(text: string) {
-    var i, j;
     var oldtext = this.editor.state.doc.toString();
     if (oldtext != text) {
       this.editor.dispatch({
         changes: { from: 0, to: this.editor.state.doc.length, insert: text }
       });
-      /*
-      // find minimum range to undo
-      for (i=0; i<oldtext.length && i<text.length && text[i] == oldtext[i]; i++) { }
-      for (j=0; j<oldtext.length && j<text.length && text[text.length-1-j] == oldtext[oldtext.length-1-j]; j++) { }
-      //console.log(i,j,oldtext.substring(i,oldtext.length-j));
-      this.replaceSelection(i, oldtext.length-j, text.substring(i, text.length-j)); // calls setCode()
-      */
-      // clear history if setting empty editor
-      if (oldtext == '') {
-        // TODO this.editor.clearHistory();
-      }
     }
   }
 

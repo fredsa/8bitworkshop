fa3385d30ceceac44b6f1ead6f6af34bcea2136c
Fred Sauer <fredsa@google.com>
2026-03-22 10:13:58 -0700

    Delete redundant clearCurrentLine
    Remove clearCurrentLine and conditional
    check from refreshDebugState as the same
    functionality is duplicated in setCurrenLine.

--- src/ide/views/editors.ts
+++ src/ide/views/editors.ts
@@ -544,11 +544,7 @@ export class SourceEditor implements ProjectView {
       return;
     }
 
-    // TODO: remove after compilation
-    this.clearCurrentLine(moveCursor);
-    if (line) {
-      this.setCurrentLine(line, moveCursor);
-    }
+    this.setCurrentLine(line, moveCursor);
   }
 
   refreshListing() {
